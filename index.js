34b867fa04ac9b233a7615e085756dcbd728025d
Sui Generis <natsynapses@gmail.com>
2020-04-13 13:40:31 -0500

    fix
    fix

--- index.js
+++ index.js
@@ -22,10 +22,6 @@ const cleakerPort = 31416;
 const { body,validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 var bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var morgan = require('morgan');
-var User = require('./models/user');
 var routes = require('./routes');
 var unicorn = "🍺🦄🍺";
 var uuid = require('node-uuid');
@@ -68,7 +64,6 @@ const server = express()
  	.use(bodyParser.json())
 	.use(forceSecure(["cleaker.me","wwww.cleaker.me"])) // FORCE SSL
 	.use(express.static(path.join(__dirname, 'server/public')))
-	.use(morgan('dev'))
 	.set('views', path.join(__dirname, 'server/views'))
 	.set('view engine', 'ejs')
 	//ROUTING Cleaker 
@@ -158,7 +153,7 @@ const server = express()
 				//console.log(pckr.cleaker); //for dev purposes, remove to not saturate the console.
 				//packet - send INFORMATION TO RUNME
 				   var activeUser = JSON.stringify({ type: "clkr_Start", cleaker: pckr.cleaker});
-				   //console.log(pckr.cleaker);
+				   console.log(pckr.cleaker);
 				   brdCstRight("runmeMasterMind", activeUser);
 					
 				}
