5ca30bbb7fb734d8e7c4d20cff6c796745ed1e40
Sui Generis <natsynapses@gmail.com>
2020-05-19 15:46:17 -0500

    models
    models

--- index.js
+++ index.js
@@ -93,30 +93,6 @@ const server = express()
 		}	
 		
 		//NODE.JS EMAILS
-		var nodemailer = require('nodemailer');
-
-		var transporter = nodemailer.createTransport({
-		  service: 'gmail',
-		  auth: {
-		    user: 'jabellae@gmail.com',
-		    pass: 'Diesel27'
-		  }
-		});
-
-		var mailOptions = {
-		  from: 'jabellae@gmail.com',
-		  to: 'jabellae@gmail.com',
-		  subject: 'Sending Email using Node.js',
-		  text: 'That was easy!'
-		};
-
-		transporter.sendMail(mailOptions, function(error, info){
-		  if (error) {
-		    console.log(error);
-		  } else {
-		    console.log('Email sent: ' + info.response);
-		  }
-		});
 		
 		
 	   /** 				  o       o                                
