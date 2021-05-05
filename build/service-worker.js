15acb548eea5bc9014f245e125644496cba4d4c2
Sebas19 <jimenezpuyasebastian@gmail.com>
2021-05-04 22:33:50 -0500

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e38c987fc50a90ab30b8667a3ff45875.js"
+  "/precache-manifest.bb350a7b6eda544dca6ea08b88d96f67.js"
 );
 
 self.addEventListener('message', (event) => {
