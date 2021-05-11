5a102ffeb322d4242485f667c4300f169bd3ecaa
jonjefemet <jonshua_97@outlook.com>
2021-05-10 20:38:32 -0500

    .

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8afdacf7e35378cc01528aa6d020173.js"
+  "/precache-manifest.edc324023a3980734d2426898c91be5a.js"
 );
 
 self.addEventListener('message', (event) => {
