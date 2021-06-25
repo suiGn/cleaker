a910540d7a37704f8656c3265dd29cd2bdf45c15
Sebas19 <jimenezpuyasebastian@gmail.com>
2021-06-25 17:37:49 -0500

    cb facebook

--- server/configs/passport.js
+++ server/configs/passport.js
@@ -120,7 +120,7 @@ module.exports = function (passport) {
   function(accessToken, refreshToken, profile, cb) {
     const { email, first_name, last_name } = profile._json;
     console.log(email);
-    done(null,profile);
+    cb(null,profile);
     // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
     //   return cb(err, user);
     // });
