95ffad9d4297f042272be392cfba3012212a0c1a
Sebas19 <jimenezpuyasebastian@gmail.com>
2021-07-26 01:04:10 -0500

    set false

--- src/App/Layout.js
+++ src/App/Layout.js
@@ -64,6 +64,7 @@ function Layout(props) {
   }, []);
 
   useEffect(() => {
+    setLoaded(true);
     props.socket.once("my_uid response", (data) => {
       setMy_Id({ id: data.user[0].u_id });
     });
