04acd741b60798470ffb0355bd50bb00b54aafd3
Sebas19 <jimenezpuyasebastian@gmail.com>
2021-09-22 23:29:29 -0500

    off camara button

--- src/App/Partials/Participant.js
+++ src/App/Partials/Participant.js
@@ -1,4 +1,5 @@
-import React,{useState,useRef,useEffect} from 'react'
+import React,{useState,useRef,useEffect} from 'react';
+import * as FeatherIcon from "react-feather";
 
 export default function Participant({roomid,socket}) {
     const [videoTracks, setVideoTracks] = useState([]);
@@ -7,26 +8,30 @@ export default function Participant({roomid,socket}) {
     const videoRef = useRef(null);
     const audioRef = useRef();
     useEffect(() => {
-        const getUserMedia = async () => {
+        const getUserMedia = async (status) => {
           try {
-            const stream = await navigator.mediaDevices.getUserMedia({video: true,audio:true});
+            const stream = await navigator.mediaDevices.getUserMedia({video: status,audio:true});
             videoRef.current.srcObject = stream;
             //socket.emit('stream',{roomid,stream});
           } catch (err) {
             console.log(err);
           }
         };
-        getUserMedia();
-    }, []);
+        getUserMedia(permision);
+    }, [permision]);
+    const checkPermissions = () =>{
+        setPermission(!permision);
+    }
     return (
         <div className="participant">
             <h3>{roomid}</h3>
             <video ref={videoRef} autoPlay={true} />
             <audio ref={audioRef} autoPlay={true} muted={true} />
             {/* <img ref={reciveRef}/> */}
-            {/* <button onClick={checkPermissions}>
-                Permissions button {permision}
-            </button> */}
+            <button onClick={checkPermissions}>
+                
+                <FeatherIcon.Video/>
+            </button> 
         </div>
     )
 }
