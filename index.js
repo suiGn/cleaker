3983e655dcb883e89627e9aa93621b07ff20eedf
Sui Gn <natsynapses@gmail.com>
2022-12-14 18:30:49 -0500

    pushing
    stuff up

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-console.log("welcome gallegos");
+console.log("Starting cleaker:");
+const details = require('./methods/details');
+//const method = require('./methods/methods');
+
+//Node Requires.
 const fs = require('fs');
-
-//passsing directoryPath and callback function
-fs.readdir('/', function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file); 
-    });
-  });
+var http = require('http');
+var os = require("os");
 
 
+let port = process.env.PORT;
+var host = os.hostname();
+if (port == null || port == "") {
+  port = 8000;
+}
 
-  
-  
+
+var httpServer = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+
+httpServer.listen(port, function() {
+    console.log("Cleaker webServer On Host: " + host +  " && Port: " + port);
+});
+ 
