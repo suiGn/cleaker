79878ef4a41e6fcaeee728fbb3b94dcd7d7291c5
Sui Gn <natsynapses@gmail.com>
2023-04-20 13:37:44 -0500

    new Cleaker
    new

---
+++ beat.js
@@ -0,0 +1,16 @@
+//every 432 ms console. log this ('beat loaded ++ s2 s2 S2 S2 S2')
+//every 432 ms console. log this ('beat loaded ++ s2 s2 S2 S2 S2')
+//every 432 ms console. log this ('beat loaded ++ s2 s2 S2 S2 S2')
+//every 432 ms console. log this ('beat loaded ++ s2 s2 S2 S2 S2')
+//every 432 ms console. log this ('beat loaded ++ s2 s2 S2 S2 S2')
+
+    // Count every 442ms and display the current count
+    var i = 0;
+    while (i < 5) {
+    setTimeout(function() {
+    console.log(i);
+    }
+    , 442 * i);
+    i++;
+    }
+
--- deploy.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# save the answer to a variable
-commit_name = raw_input("Commit Name: ")
-os.system("git add .")
-os.system("git commit -m \""+ commit_name + "\"")
-os.system("git push heroku master")
-os.system("heroku open")
