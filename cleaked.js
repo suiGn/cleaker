1f78a7082d901d428c2e67c002f582c2167e4822
Sui Gn <natsynapses@gmail.com>
2023-08-10 00:56:46 -0500

    it gets cleaked.js

--- cleaked.js
+++ cleaked.js
@@ -1,3 +1,14 @@
+//cleaked.js
+console.log(`
+    \ | /
+    '  _  '
+   -  |_|  -
+    ' | | '
+    _,_|___
+   |   _ []|
+   |  (O)  |
+   |_______|it gets cleaked.
+  `);
 const Cleaker = require('./cleaker');
 const myCleaker = new Cleaker();
 console.log(myCleaker);
